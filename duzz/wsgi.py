a73ab2a124c05c7148be43b8b936a5dda8f7e2d2
Giorgos Logiotatidis <seadog@sealabs.net>
2014-03-09 12:52:22 +0200

    WSGI to server media as well.

--- duzz/wsgi.py
+++ duzz/wsgi.py
@@ -22,6 +22,6 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "duzz.settings")
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+from dj_static import Cling, MediaCling
 
-application = Cling(get_wsgi_application())
+application = Cling(MediaCling(get_wsgi_application()))
